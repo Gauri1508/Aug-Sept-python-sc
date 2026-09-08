0b2d2feca20f3c563f27f53491c26270001a5f61
Gauri1508 <86878150+Gauri1508@users.noreply.github.com>
2026-09-08 11:19:06 +0530

    Create f the x.py
    F#ck with x, hahahah just kiddin uhm just the F

---
+++ f the x.py
@@ -0,0 +1,16 @@
+'''
+xxxxx
+xx
+xxxxx
+xx
+xx
+
+for x in range(2):
+    print("xxxxx")
+    print("xx")
+    for y in range(1):
+        print("xx")
+'''
+numbers = [5, 2, 5, 2, 2]
+for n in numbers:
+    print( "x"* n)
